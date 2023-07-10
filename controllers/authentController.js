9b1f4c0da9861ac6fc6b30800d3632ce943cf6a0
eugeniabut <eugeniabutkevich2022@gmail.com>
2023-07-10 14:21:19 +0200

    passwordChandeHandler added

--- controllers/authentController.js
+++ controllers/authentController.js
@@ -54,3 +54,30 @@ export const loginHandler = async (req, res, next) => {
   }
 };
 
+export const passwordChangeHandler=async(req,res,next)=>{
+
+  try{
+      const userId =req.params.id
+  const {email,currentPassword, confirmPassword, newPassword} = req.body
+  
+  if(confirmPassword !== newPassword) return res.status(400).send("Invalid Credentials")
+
+  const userRecord = await User.findById(userId)
+  if(userRecord === null) return res.status(401).send("Invalid Credentials, Record not found")
+
+  const isValid = await bcrypt.compare(currentPassword, userRecord.password)
+
+  if(!isValid) return res.status(401).send("Invalid Credentials")
+
+  const salt = await bcrypt.genSalt(11)
+  const newHashedPassword = await bcrypt.hash(newPassword, salt)
+
+  const result = await User.findByIdAndUpdate(userId, {password:newHashedPassword})
+
+  res.status(202).send("Password Changed Successfully")
+  }
+  catch(err){
+      res.status(401).send("Something went wrong...! ")
+  }
+
+}
